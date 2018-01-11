ace705a0ec372624503e499aeb473ece5ab891bd
siddhism <siddhesh.it@gmail.com>
2018-01-11 18:13:44 +0530

    login required

--- fbwall/settings.py
+++ fbwall/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
 
     'post',
     'api',
--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
+from django.contrib.auth.decorators import login_required
+
 from .views import *
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^$', login_required(IndexView.as_view()), name='index'),
     url(r'^create-post/$', CreatePostView.as_view(), name='create_post'),
 ]
